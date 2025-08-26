5fd9a30d354d48201264c25245d0c70fb42a428a
saikrishna gidugu <gidugusaikrishna@gmail.com>
2025-08-26 15:49:53 +0000

    google llm for english,hindi

--- src/configs/hindi_config.py
+++ src/configs/hindi_config.py
@@ -1,17 +1,30 @@
-from livekit.plugins import openai, silero, sarvam
+from livekit.plugins import openai, silero, sarvam,google
 from livekit.plugins.turn_detector.multilingual import MultilingualModel
 import os
 
 def get_config():
     return dict(
-        stt=sarvam.STT(model="saarika:v2.5", language="hi-IN"),
-        llm=openai.LLM(
-            model="gpt-4o",   # 👈 note the "openai/" prefix (OpenRouter convention)
+
+	llm=google.beta.realtime.RealtimeModel(
+        model="gemini-2.0-flash-exp",
+        voice="Puck",
+        temperature=0.5,
+	 language="hi-IN"),
+
+
+
+
+
+
+
+#        stt=sarvam.STT(model="saarika:v2.5", language="hi-IN"),
+#        llm=openai.LLM(
+#            model="gpt-4o",   # 👈 note the "openai/" prefix (OpenRouter convention)
           #  base_url="https://openrouter.ai/api/v1",  # 👈 route through OpenRouter
           #  api_key=os.getenv("OPENROUTER_API_KEY"), 
-            api_key=os.getenv("OPENAI_API_KEY"), 
-        ),
-        tts=sarvam.TTS(target_language_code="hi-IN", speaker="abhilash"),
+#            api_key=os.getenv("OPENAI_API_KEY"), 
+#        ),
+#        tts=sarvam.TTS(target_language_code="hi-IN", speaker="abhilash"),
         vad=silero.VAD.load(),
         turn_detection=MultilingualModel(),
         use_tts_aligned_transcript=True,
--- src/configs/kannada_config.py
+++ src/configs/kannada_config.py
@@ -1,17 +1,29 @@
-from livekit.plugins import openai, silero, sarvam
+from livekit.plugins import openai, silero, sarvam,google
 from livekit.plugins.turn_detector.multilingual import MultilingualModel
 import os
 
 def get_config():
     return dict(
-        stt=sarvam.STT(model="saarika:v2.5", language="kn-IN"),
-        llm=openai.LLM(
-            model="gpt-4o",               # 👈 include provider prefix for OpenRouter
+#        stt=sarvam.STT(model="saarika:v2.5", language="kn-IN"),
+#        llm=openai.LLM(
+#            model="gpt-4o",               # 👈 include provider prefix for OpenRouter
        #     base_url="https://openrouter.ai/api/v1", # 👈 force it to hit OpenRouter API
         #    api_key=os.getenv("OPENROUTER_API_KEY"), 
-            api_key=os.getenv("OPENAI_API_KEY"), 
-        ),
-        tts=sarvam.TTS(target_language_code="kn-IN", speaker="abhilash"),
+#            api_key=os.getenv("OPENAI_API_KEY"), 
+#        ),
+#        tts=sarvam.TTS(target_language_code="kn-IN", speaker="abhilash"),
+ 
+	llm=google.beta.realtime.RealtimeModel(
+        model="gemini-2.0-flash-exp",
+        voice="Puck",
+        temperature=0.5,
+	language="kn-IN"),
+
+
+
+
+
+
         vad=silero.VAD.load(),
         turn_detection=MultilingualModel(),
         use_tts_aligned_transcript=True,
